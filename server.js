2183339c447b8a82b1bc65d4c0bb76ac0f0377e6
romeidy <jay.wardhana@hotmail.com>
2017-03-25 11:27:55 +0700

    express listen commit fix

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express();
 
-cons PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, function{
+
+
+app.listen(PORT, function(){
   console.log("It's work");
 });
