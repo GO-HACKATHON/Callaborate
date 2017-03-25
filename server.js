b598f2b4a40b0659fd626335fc6dc2fa0d23231b
romeidy <jay.wardhana@hotmail.com>
2017-03-25 11:38:51 +0700

    configure express n mongo

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
-var express = require('express');
+var express     = require('express'),
+    ejs         = require('ejs'),
+    engine      = require('ejs-mate'),
+    session     = require('express-session'),
+    mongoose    = require('mongoose'),
+    MongoStore  = require('connect-mongo')(session);
 
 var app = express();
-
 const PORT = process.env.PORT || 3000;
 
+mongoose.connect('mongodb://callaborate:admin@ds117859.mlab.com:17859/callaboratedb');
+
+app.use(express.static('public'));
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.use(session({
+  secret: 'secretkey',
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
 
 
 app.listen(PORT, function(){
