7cb9944a9f5b7fdd304ad7a4c7e9b53adb5b2101
romeidy <jay.wardhana@hotmail.com>
2017-03-25 11:50:11 +0700

    fix mongo n express conf

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-var express     = require('express'),
-    ejs         = require('ejs'),
-    engine      = require('ejs-mate'),
-    session     = require('express-session'),
-    mongoose    = require('mongoose'),
-    MongoStore  = require('connect-mongo')(session);
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var engine = require('ejs-mate');
+var session = require('express-session');
+var mongoose = require('mongoose');
+var MongoStore = require('connect-mongo')(session);
+
 
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://callaborate:admin@ds117859.mlab.com:17859/callaboratedb');
+mongoose.connect('mongodb://callaborate:admin@ds035643.mlab.com:35643/callaborate-test');
 
 app.use(express.static('public'));
 app.engine('ejs', engine);
-app.set('view enginejs', 'ejs');
+app.set('view engine', 'ejs');
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
@@ -24,7 +27,13 @@ app.use(session({
   store: new MongoStore({mongooseConnection: mongoose.connection})
 }));
 
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
+app.get('/register', (req, res, next) => {
+  res.render('user/register');
+});
 
 app.listen(PORT, function(){
-  console.log("It's work");
+  console.log("App running");
 });
